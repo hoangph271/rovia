c622808c9079144e97c76616aedd71f0f07c5bda
Snêu <hoangph271@gmail.com>
2023-07-12 22:57:47 +0700

    .prettierrc

--- src/app.tsx
+++ src/app.tsx
@@ -2,17 +2,11 @@ import useAsset from "ultra/hooks/use-asset.js";
 // Twind
 import { tw } from "./twind/twind.ts";
 
-export default function App() {
+export default function App () {
   console.log("Hello world!");
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>Ultra</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
-        <link rel="stylesheet" href={useAsset("/style.css")} />
-      </head>
+      <Head />
       <body>
         <main>
           <h1 className={tw(`text-2xl font-mono margin mb-8`)}>
@@ -39,3 +33,13 @@ export default function App() {
     </html>
   );
 }
+
+const Head = () => (
+  <head>
+    <meta charSet="utf-8" />
+    <title>Ultra</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href={useAsset("/favicon.ico")} />
+    <link rel="stylesheet" href={useAsset("/style.css")} />
+  </head>
+)
